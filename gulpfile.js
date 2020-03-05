97fe53b9f4bf0c98d9a813a3b0ec9f5532b4b5ac
olegbaskakov <you@example.com>
2020-03-05 23:13:49 +0300

    feat add futura Book and DemiC fonts

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const {src, dest, watch} = require('gulp')
 const browserSync = require('browser-sync').create()
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer');
 
 
 // Static server
@@ -13,12 +14,16 @@ const sass = require('gulp-sass');
   });
   watch("./*.html").on('change', browserSync.reload);
   watch("./sass/**/*.sass", serveSass);
+  watch("./sass/**/*.scss", serveSass);
   watch("./js/*.js").on('change', browserSync.reload);
 };
 
 function serveSass() {
-  return src('./sass/*.sass')
-  .pipe(sass())
+  return src('./sass/**/*.sass', './sass/**/*.scss')
+    .pipe(sass())
+    .pipe(autoprefixer({
+      cascade: false
+  }))
     .pipe(dest('./css'))
     .pipe(browserSync.stream());
 };

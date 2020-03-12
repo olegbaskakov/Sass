fa5f7a7b8eb5fb48d5a2eccc5ce2156c9a6c9422
olegbaskakov <you@example.com>
2020-03-12 13:10:13 +0300

    feat form

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   const modal = document.querySelector('.modal');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
   const closeBtn = document.querySelector('.modal__close');
+  const closeEsc = document.querySelector('keyCode=27');
   const switchModal = () => {
     modal.classList.toggle('modal--visible');
   }
@@ -11,5 +12,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
   });
 
   closeBtn.addEventListener('click', switchModal);
+  closeEsc.addEventListener('click', switchModal);
 
 });

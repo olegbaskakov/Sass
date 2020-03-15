41ffceee80c0aff068cc193e745aecee77eff752
olegbaskakov <you@example.com>
2020-03-15 20:22:20 +0300

    feat button up

--- js/main.js
+++ js/main.js
@@ -47,4 +47,15 @@ $(document).ready(function () {
     }
   });
 
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+        if ($('.upbutton').is(':hidden')) {
+            $('.upbutton').css({opacity : 1}).fadeIn('slow');
+        }
+    } else { $('.upbutton').stop(true, false).fadeOut('fast'); }
+  });
+  $('.upbutton').click(function() {
+    $('html, body').stop().animate({scrollTop : 0}, 300);
+  });
+
 });
